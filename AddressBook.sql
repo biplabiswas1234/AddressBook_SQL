81ec89fd4c66c16bec36fe76360a104a6c7d728d
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-15 10:57:18 +0530

    [Biplab] Add: Creating table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,7 +1,18 @@
 create database AddressBookService
 use AddressBookService
 
-
+create table AddressBook
+(
+	ID int identity(1,1) primary key,
+	FirstName varchar(20) not null,
+	LastName varchar(20) not null,
+	Address varchar(100) not null,
+	City varchar(20) not null,
+	State varchar(20) not null,
+	Zip bigint not null,
+	PhoneNumber bigint not null,
+	Email varchar(50) not null
+);
 
 
 
